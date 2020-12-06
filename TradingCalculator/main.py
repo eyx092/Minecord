bfd3d64a290f75afdfbaa5125f9c7d1f0c524eea
eyx092 <72813338+eyx092@users.noreply.github.com>
2020-12-06 18:42:06 -0500

    Add snow calculations

--- TradingCalculator/main.py
+++ TradingCalculator/main.py
@@ -10,47 +10,78 @@ print("Press Control-C to quit.")
 def initialize():
     global emerald_value
     global lapis_value
+    global snow_value
     emerald_value = int(input("Set emerald value: "))
     lapis_value = int(input("Set lapis value: "))
+    snow_value = int(input("Set snow value: "))
 
 
 def calculate_emerald_total():
     global amount
     global total
     global est_total
-    global items_total
+    global lapis_total
+    global snow_total
     amount = int(input("Input amount: "))
     total = amount * emerald_value
     est_total = ceil(round(total, -3))
-    items_total = round(total / lapis_value)
+    lapis_total = round(total / lapis_value)
+    snow_total = round(total / snow_value)
 
 
 def calculate_lapis_total():
     global amount
     global total
     global est_total
-    global items_total
+    global emeralds_total
+    global snow_total
     amount = int(input("Input amount: "))
     total = amount * lapis_value
     est_total = ceil(round(total, -3))
-    items_total = round(total / emerald_value)
+    emeralds_total = round(total / emerald_value)
+    snow_total = round(total / snow_value)
+
+def calculate_snow_total():
+    global amount
+    global total
+    global est_total
+    global emeralds_total
+    global lapis_total
+    amount = int(input("Input amount: "))
+    total = amount * snow_value
+    est_total = ceil(round(total, -3))
+    emeralds_total = round(total / emerald_value)
+    lapis_total = round(total / lapis_value)
 
 def output(item):
     print("\n")
+    print("```")
     print("Calculation")
     print("===================")
     if item == 1:
         print("Lapis amount:", (amount))
     if item == 2:
         print("Emerald amount:", (amount))
+    if item == 3:
+        print("Snow amount:", (amount))
     print("Real Total:",(total))
     print("Rounded Total:",(est_total))
     if item == 1:
-        print("Total in emeralds: ~",(items_total),sep="")
+        print("Ballpark Total:", amount*35000)
     if item == 2:
-        print("Total in lapis: ~",(items_total),sep="")
+        print("Ballpark Total:", amount*3000)
+    if item == 1:
+        print("Total in emeralds:", (emeralds_total))
+        print("Total in snow:", (snow_total))
+    if item == 2:
+        print("Total in lapis:", (lapis_total))
+        print("Total in snow:", (snow_total))
+    if item == 3:
+        print("Total in emeralds:", (emeralds_total))
+        print("Total in lapis:", (lapis_total))
     print("====================")
     print("Calculation end.")
+    print("```")
     print("\n")
 
 try:
@@ -58,6 +89,7 @@ try:
     while program_state == 1:
         print("1. Calculate for emeralds")
         print("2. Calculate for lapis")
+        print("3. Calculate for snow")
         option = input("Choose an option: ")
         if option == "1":
             calculate_emerald_total()
@@ -65,6 +97,9 @@ try:
         elif option == "2":
             calculate_lapis_total()
             output(1)
+        elif option == "3":
+            calculate_snow_total()
+            output(3)
         else:
             print("Not an option.")
 
